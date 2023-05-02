5f94a0f57526ba0b23998424cba2ae4675934d77
Panferov.creator <panferov.creator@gmail.com>
2023-05-02 20:31:14 +0500

    Initial Commit

---
+++ AnimationApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimationApp
+//
+//  Created by Dmitriy Panferov on 02/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
